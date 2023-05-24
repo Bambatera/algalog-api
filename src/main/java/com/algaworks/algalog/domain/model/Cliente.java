5d2832804cef7ad0d021b3890280b19f29011dc4
Leandro Menezes <lmsilv@gmail.com>
2023-05-24 00:35:18 -0300

    Usando Jakarta Persistence JPA
    Ajuste no `serverTimezone`, modificado de UTC para GMT-0 devido a versão do MySQL.

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,14 +1,28 @@
 package com.algaworks.algalog.domain.model;
 
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author leand
  */
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String nome;
+    
     private String email;
+    
+    @Column(name = "fone")
     private String telefone;
 
     public Cliente() {
@@ -48,6 +62,28 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return Objects.equals(this.id, other.id);
     }
 
 }
